5129128991dd9b57300dc9b4bd1661b91f0329a1
Prince Tiwari <dbvertexdesigner@gmail.com>
2026-02-21 18:41:51 +0530

    fdf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,3 @@
 export const environment = {
-<<<<<<< HEAD
   apiUrl: 'https://api.cavalierlogistic.graphicsvolume.com/api'
-=======
-  production: true,
-  apiUrl: 'http://localhost:5000/api' // Peeche se slash hata diya hai taaki route errors na aayein
->>>>>>> ebbcea219a6692158973ba61032c3178f3738dfd
 };

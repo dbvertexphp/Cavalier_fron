53167c7f7a6b4caffa1324ffb0fc661a78f0bccd
CorrectUsername <correctemail@example.com>
2026-03-14 11:37:32 +0530

    dfa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   // Swagger wala URL yahan daalna hai
-  apiUrl: 'http://localhost:5000/api' 
+  // apiUrl: 'http://localhost:5000/api'
+  apiUrl: 'https://api.cavalierlogistic.graphicsvolume.com/api', // Live URL
 };

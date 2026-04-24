631b47f15b725913c8804ea7c0e912b49dda7eb9
Prince Tiwari <princetiwari6044@gmail.comm>
2026-04-24 17:17:49 +0530

    gasd

--- src/app/pages/inquiry/inquiry.component.ts
+++ src/app/pages/inquiry/inquiry.component.ts
@@ -1135,7 +1135,7 @@ openDimModal() {
         customerName: '', 
         branchName: 'MAIN', 
         receivedDate: new Date().toISOString().split('T')[0], 
-        location: 'DELHI', 
+        location: '', 
         transportMode: 'Air', 
         shipmentType: 'International',
         lineOfBusinessId: null,

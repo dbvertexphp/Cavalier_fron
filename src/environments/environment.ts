4e371bda21e491d54e8eb72df6beb89fe21f6b8b
Prince Tiwari <princetiwari6044@gmail.comm>
2026-05-01 10:52:22 +0530

    changed now

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.cavalierlogistic.graphicsvolume.com/api',  // Live URL
+  // apiUrl: 'https://api.cavalierlogistic.graphicsvolume.com/api',  // Live URL
   
-  // apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'http://localhost:5000/api',
   firebase: {
     apiKey: "AIzaSyD8ys-ve3XONsOky2FVTVOvr3S6V5vN5vg",
   authDomain: "cavalierfron.firebaseapp.com",
